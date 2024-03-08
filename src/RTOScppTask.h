c5969eff03a23fd612693b172fa4fc3dff0e3a75
alkonosst <m.ramirez.bravo.94@gmail.com>
2024-03-08 18:08:57 -0300

    feat: Add dynamic Tasks and more methods

--- src/RTOScppTask.h
+++ src/RTOScppTask.h
@@ -12,8 +12,8 @@
 
 class TaskBase {
   private:
-  TaskBase(const TaskBase&)       = delete;
-  void operator=(const TaskBase&) = delete;
+  TaskBase(const TaskBase&)       = delete; // Delete copy constructor
+  void operator=(const TaskBase&) = delete; // Delete copy assignment operator
 
   protected:
   TaskBase(const char* name, TaskFunction_t function, uint8_t priority)
@@ -21,7 +21,12 @@ class TaskBase {
       , _function(function)
       , _priority(priority)
       , _handle(nullptr) {}
-  ~TaskBase() { vTaskDelete(_handle); }
+
+  virtual ~TaskBase() {
+    if (_handle) {
+      vTaskDelete(_handle);
+    }
+  }
 
   const char* _name;
   TaskFunction_t _function;
@@ -30,6 +35,20 @@ class TaskBase {
 
   public:
   virtual bool init() = 0;
+
+  void suspend() { vTaskSuspend(_handle); }
+  void resume() { vTaskResume(_handle); }
+  bool abortDelay() { return xTaskAbortDelay(_handle); }
+
+  void setPriority(uint8_t priority) {
+    _priority = priority;
+    vTaskPrioritySet(_handle, _priority);
+  }
+
+  uint8_t getPriority() { return uxTaskPriorityGet(_handle); }
+  uint8_t getPriorityFromISR() { return uxTaskPriorityGetFromISR(_handle); }
+  const char* getName() { return _name; }
+  TaskHandle_t getHandle() { return _handle; }
 
   bool notify(uint32_t value, eNotifyAction action) { return xTaskNotify(_handle, value, action); }
 
@@ -59,6 +78,52 @@ class TaskBase {
   }
 };
 
+class TaskDynamic : public TaskBase {
+  public:
+  TaskDynamic(const char* name, TaskFunction_t function, uint8_t priority, uint32_t stack_size)
+      : TaskBase(name, function, priority)
+      , _stack_size(stack_size) {}
+
+  bool init() { return xTaskCreate(_function, _name, _stack_size, nullptr, _priority, &_handle); }
+
+  private:
+  uint32_t _stack_size;
+};
+
+class TaskDynamicPinnedToCore : public TaskBase {
+  public:
+  TaskDynamicPinnedToCore(const char* name, TaskFunction_t function, uint8_t priority,
+                          uint32_t stack_size, BaseType_t running_core)
+      : TaskBase(name, function, priority)
+      , _stack_size(stack_size)
+      , _running_core(running_core) {}
+
+  bool init() {
+    return xTaskCreatePinnedToCore(
+      _function, _name, _stack_size, nullptr, _priority, &_handle, _running_core);
+  }
+
+  private:
+  uint32_t _stack_size;
+  BaseType_t _running_core;
+};
+
+template <uint32_t STACK_SIZE>
+class TaskStatic : public TaskBase {
+  public:
+  TaskStatic(const char* name, TaskFunction_t function, uint8_t priority)
+      : TaskBase(name, function, priority) {}
+
+  bool init() {
+    _handle = xTaskCreateStatic(_function, _name, STACK_SIZE, nullptr, _priority, _stack, &_tcb);
+    return _handle != nullptr ? true : false;
+  }
+
+  private:
+  StaticTask_t _tcb;
+  StackType_t _stack[STACK_SIZE];
+};
+
 template <uint32_t STACK_SIZE>
 class TaskStaticPinnedToCore : public TaskBase {
   public:
@@ -68,14 +133,8 @@ class TaskStaticPinnedToCore : public TaskBase {
       , _running_core(running_core) {}
 
   bool init() {
-    _handle = xTaskCreateStaticPinnedToCore(_function,      // Función
-                                            _name,          // Nombre
-                                            STACK_SIZE,     // Tamaño stack
-                                            NULL,           // Parámetros
-                                            _priority,      // Prioridad
-                                            _stack,         // Buffer stack
-                                            &_tcb,          // Buffer tarea
-                                            _running_core); // Core
+    _handle = xTaskCreateStaticPinnedToCore(
+      _function, _name, STACK_SIZE, nullptr, _priority, _stack, &_tcb, _running_core);
 
     return _handle != nullptr ? true : false;
   }
