70c75ff6b691766685e1ad6f44a8c7cba6efdec2
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-01-10 19:20:14 -0300

    refactor: Change timer structure

--- src/RTOScppTimer.h
+++ src/RTOScppTimer.h
@@ -9,128 +9,172 @@
 #include <Arduino.h>
 #include <freertos/timers.h>
 
-class TimerInterface {
+namespace RTOS::Timers {
+
+// Interface for Timer objects, useful when using pointers
+class ITimer {
   protected:
-  TimerInterface(TimerHandle_t handle)
-      : _handle(handle) {}
+  ITimer() = default;
 
+  public:
+  ITimer(const ITimer&)            = delete;
+  ITimer& operator=(const ITimer&) = delete;
+  ITimer(ITimer&&)                 = delete;
+  ITimer& operator=(ITimer&&)      = delete;
+
+  virtual ~ITimer() = default;
+
+  virtual TimerHandle_t getHandle() const = 0;
+
+  virtual bool create(const char* name, TimerCallbackFunction_t callback, const TickType_t period,
+                      void* id, const bool auto_reload, const bool start) = 0;
+
+  virtual bool start(const TickType_t ticks_to_wait = portMAX_DELAY) const = 0;
+  virtual bool startFromISR(BaseType_t& task_woken) const                  = 0;
+  virtual bool stop(const TickType_t ticks_to_wait = portMAX_DELAY) const  = 0;
+  virtual bool stopFromISR(BaseType_t& task_woken) const                   = 0;
+
+  virtual bool isActive() const = 0;
+
+  virtual bool reset(const TickType_t ticks_to_wait = portMAX_DELAY) const = 0;
+  virtual bool resetFromISR(BaseType_t& task_woken) const                  = 0;
+
+  virtual const char* getName() const      = 0;
+  virtual TickType_t getExpiryTime() const = 0;
+
+  virtual bool setPeriod(const TickType_t period,
+                         const TickType_t ticks_to_wait = portMAX_DELAY) const         = 0;
+  virtual bool setPeriodFromISR(const TickType_t period, BaseType_t& task_woken) const = 0;
+  virtual TickType_t getPeriod() const                                                 = 0;
+
+  virtual void setTimerID(void* id) const = 0;
+  virtual void* getTimerID() const        = 0;
+
+  virtual void setReloadMode(const bool auto_reload) const = 0;
+  virtual bool getReloadMode() const                       = 0;
+
+  virtual explicit operator bool() const = 0;
+};
+
+namespace Internal {
+
+// CRTP base class template
+template <typename Derived>
+class Policy {
+  public:
+  TimerHandle_t getHandle() const { return _handle; }
+
+  bool create(const char* name, TimerCallbackFunction_t callback, const TickType_t period, void* id,
+              const bool auto_reload, const bool start) {
+    return static_cast<Derived*>(this)->createImpl(name, callback, period, id, auto_reload, start);
+  }
+
+  protected:
   TimerHandle_t _handle;
-  const char* _name;
-  TimerCallbackFunction_t _callback;
-  TickType_t _period;
-  void* _id;
-  bool _auto_reload;
-  bool _start;
-
-  public:
-  virtual ~TimerInterface() {
-    if (_handle) vTaskDelete(_handle);
-  }
-
-  TimerInterface(const TimerInterface&)                = delete;
-  TimerInterface& operator=(const TimerInterface&)     = delete;
-  TimerInterface(TimerInterface&&) noexcept            = delete;
-  TimerInterface& operator=(TimerInterface&&) noexcept = delete;
-
-  void setup(const char* name, const TimerCallbackFunction_t callback, const TickType_t period,
-             void* id, const bool auto_reload, const bool start) {
-    _name        = name;
-    _callback    = callback;
-    _period      = period;
-    _id          = id;
-    _auto_reload = auto_reload;
-    _start       = start;
-  }
-
-  virtual bool create() = 0;
-
-  bool start(const TickType_t ticks_to_wait = portMAX_DELAY) const {
-    return xTimerStart(_handle, ticks_to_wait);
-  }
-
-  bool startFromISR(BaseType_t& task_woken) const {
-    return xTimerStartFromISR(_handle, &task_woken);
-  }
-  bool stop(const TickType_t ticks_to_wait = portMAX_DELAY) const {
-    return xTimerStop(_handle, ticks_to_wait);
-  }
-  bool stopFromISR(BaseType_t& task_woken) const { return xTimerStopFromISR(_handle, &task_woken); }
-
-  bool isActive() const { return xTimerIsTimerActive(_handle); }
-
-  bool reset(const TickType_t ticks_to_wait = portMAX_DELAY) const {
-    return xTimerReset(_handle, ticks_to_wait);
-  }
-
-  bool resetFromISR(BaseType_t& task_woken) const {
-    return xTimerResetFromISR(_handle, &task_woken);
-  }
-
-  const char* getName() const { return pcTimerGetName(_handle); }
-  TickType_t getExpiryTime() const { return xTimerGetExpiryTime(_handle); }
-
-  bool setPeriod(const TickType_t period, const TickType_t ticks_to_wait = portMAX_DELAY) const {
-    return xTimerChangePeriod(_handle, period, ticks_to_wait);
-  }
-
-  bool setPeriodFromISR(const TickType_t period, BaseType_t& task_woken) const {
-    return xTimerChangePeriodFromISR(_handle, period, &task_woken);
-  }
-
-  TickType_t getPeriod() const { return xTimerGetPeriod(_handle); }
-
-  void setTimerID() const { vTimerSetTimerID(_handle, _id); }
-  void* getTimerID() const { return pvTimerGetTimerID(_handle); }
-
-  void setReloadMode(const bool auto_reload) const { vTimerSetReloadMode(_handle, auto_reload); }
-  bool getReloadMode() const { return uxTimerGetReloadMode(_handle); }
-
-  explicit operator bool() const { return _handle != nullptr; }
 };
 
-class TimerDynamic : public TimerInterface {
+// Policy for timer with dynamic memory allocation
+class DynamicPolicy : public Policy<DynamicPolicy> {
   public:
-  TimerDynamic()
-      : TimerInterface(nullptr) {}
-
-  TimerDynamic(const char* name, const TimerCallbackFunction_t callback, const TickType_t period,
-               void* id, const bool auto_reload, const bool start)
-      : TimerInterface(xTimerCreate(name, period, auto_reload, id, callback)) {
-    this->setup(name, callback, period, id, auto_reload, start);
-
-    if ((_handle != nullptr) && start) {
-      this->start();
-    }
-  }
-
-  bool create() {
-    _handle = xTimerCreate(_name, _period, _auto_reload, _id, _callback);
-    if (_handle == nullptr) return false;
-    return _start ? this->start() : true;
+  bool createImpl(const char* name, TimerCallbackFunction_t callback, const TickType_t period,
+                  void* id, const bool auto_reload, const bool start) {
+    this->_handle = xTimerCreate(name, period, auto_reload, id, callback);
+    return this->_handle != nullptr;
   }
 };
 
-class TimerStatic : public TimerInterface {
+// Policy for timer with static memory allocation
+class StaticPolicy : public Policy<StaticPolicy> {
   public:
-  TimerStatic()
-      : TimerInterface(nullptr) {}
-
-  TimerStatic(const char* name, const TimerCallbackFunction_t callback, const TickType_t period,
-              void* id, const bool auto_reload, const bool start)
-      : TimerInterface(xTimerCreateStatic(name, period, auto_reload, id, callback, &_tcb)) {
-    this->setup(name, callback, period, id, auto_reload, start);
-
-    if ((_handle != nullptr) && start) {
-      this->start();
-    }
-  }
-
-  bool create() {
-    _handle = xTimerCreateStatic(_name, _period, _auto_reload, _id, _callback, &_tcb);
-    if (_handle == nullptr) return false;
-    return _start ? this->start() : true;
+  bool createImpl(const char* name, TimerCallbackFunction_t callback, const TickType_t period,
+                  void* id, const bool auto_reload, const bool start) {
+    this->_handle = xTimerCreateStatic(name, period, auto_reload, id, callback, &_tcb);
+    return this->_handle != nullptr;
   }
 
   private:
   StaticTimer_t _tcb;
 };
+
+// Main Timer class. You need to specify the policy used
+template <typename Policy>
+class Timer : public ITimer {
+  public:
+  Timer() {}
+
+  Timer(const char* name, TimerCallbackFunction_t callback, const TickType_t period, void* id,
+        const bool auto_reload, const bool start) {
+    _policy.createImpl(name, callback, period, id, auto_reload, start);
+  }
+
+  ~Timer() {
+    if (getHandle()) xTimerDelete(_policy.getHandle(), portMAX_DELAY);
+  }
+
+  TimerHandle_t getHandle() const override { return _policy.getHandle(); }
+
+  bool create(const char* name, TimerCallbackFunction_t callback, const TickType_t period, void* id,
+              const bool auto_reload, const bool start) override {
+    return _policy.createImpl(name, callback, period, id, auto_reload, start);
+  }
+
+  bool start(const TickType_t ticks_to_wait = portMAX_DELAY) const override {
+    return xTimerStart(getHandle(), ticks_to_wait);
+  }
+
+  bool startFromISR(BaseType_t& task_woken) const override {
+    return xTimerStartFromISR(getHandle(), &task_woken);
+  }
+
+  bool stop(const TickType_t ticks_to_wait = portMAX_DELAY) const override {
+    return xTimerStop(getHandle(), ticks_to_wait);
+  }
+
+  bool stopFromISR(BaseType_t& task_woken) const override {
+    return xTimerStopFromISR(getHandle(), &task_woken);
+  }
+
+  bool isActive() const override { return xTimerIsTimerActive(getHandle()); }
+
+  bool reset(const TickType_t ticks_to_wait = portMAX_DELAY) const override {
+    return xTimerReset(getHandle(), ticks_to_wait);
+  }
+
+  bool resetFromISR(BaseType_t& task_woken) const override {
+    return xTimerResetFromISR(getHandle(), &task_woken);
+  }
+
+  const char* getName() const override { return pcTimerGetName(getHandle()); }
+  TickType_t getExpiryTime() const override { return xTimerGetExpiryTime(getHandle()); }
+
+  bool setPeriod(const TickType_t period,
+                 const TickType_t ticks_to_wait = portMAX_DELAY) const override {
+    return xTimerChangePeriod(getHandle(), period, ticks_to_wait);
+  }
+
+  bool setPeriodFromISR(const TickType_t period, BaseType_t& task_woken) const override {
+    return xTimerChangePeriodFromISR(getHandle(), period, &task_woken);
+  }
+
+  TickType_t getPeriod() const override { return xTimerGetPeriod(getHandle()); }
+
+  void setTimerID(void* id) const override { vTimerSetTimerID(getHandle(), id); }
+  void* getTimerID() const override { return pvTimerGetTimerID(getHandle()); }
+
+  void setReloadMode(const bool auto_reload) const override {
+    vTimerSetReloadMode(getHandle(), auto_reload);
+  }
+  bool getReloadMode() const override { return uxTimerGetReloadMode(getHandle()); }
+
+  explicit operator bool() const override { return getHandle() != nullptr; }
+
+  private:
+  Policy _policy;
+};
+
+} // namespace Internal
+
+using TimerDynamic = Internal::Timer<Internal::DynamicPolicy>;
+using TimerStatic  = Internal::Timer<Internal::StaticPolicy>;
+
+} // namespace RTOS::Timers
