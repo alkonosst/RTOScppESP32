b178b7a96317fdefcf52ca873058fa1ee36f8d4f
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-01-11 20:02:22 -0300

    fix: Fix double task creation

--- src/RTOScppTask.h
+++ src/RTOScppTask.h
@@ -30,7 +30,9 @@ class ITask {
   virtual bool isCreated() const                                   = 0;
   virtual void suspend() const                                     = 0;
   virtual void resume() const                                      = 0;
-  virtual bool abortDelay() const                                  = 0;
+  virtual eTaskState getState() const                              = 0;
+
+  virtual bool abortDelay() const = 0;
 
   virtual TaskHandle_t getHandle() const = 0;
   virtual const char* getName() const    = 0;
@@ -54,7 +56,7 @@ class ITask {
   virtual uint32_t notifyTake(const bool clear, const TickType_t ticks_to_wait) const = 0;
 
   virtual bool notifyWait(const uint32_t clear_on_entry, const uint32_t clear_on_exit,
-                          uint32_t* const value, const TickType_t ticks_to_wait) const = 0;
+                          uint32_t& value, const TickType_t ticks_to_wait) const = 0;
 
   virtual void updateStackStats()          = 0;
   virtual uint32_t getStackUsed() const    = 0;
@@ -68,31 +70,37 @@ namespace Internal {
 
 // CRTP base policy class
 template <typename Derived>
-struct Policy {
+class Policy {
+  public:
+  TaskHandle_t getHandle() const { return _handle; }
+  const char* getName() const { return _name; }
+  void* getParameters() const { return _parameters; }
+  uint8_t getCore() const { return _core; }
+
+  void setup(const char* name, TaskFunction_t function, uint8_t priority, void* parameters,
+             BaseType_t core) {
+    _name       = name;
+    _parameters = parameters;
+    _function   = function;
+    _priority   = priority;
+    _core       = core;
+  }
+
+  bool create() { return static_cast<Derived*>(this)->createImpl(); }
+
+  protected:
   TaskHandle_t _handle;
   const char* _name;
   TaskFunction_t _function;
   uint8_t _priority;
   void* _parameters;
-  BaseType_t _core;
-
-  bool create() { return static_cast<Derived*>(this)->createImpl(); }
-
-  bool create(const char* name, TaskFunction_t function, void* parameters, uint8_t priority,
-              BaseType_t core) {
-    _name       = name;
-    _function   = function;
-    _parameters = parameters;
-    _priority   = priority;
-    _core       = core;
-
-    return static_cast<Derived*>(this)->createImpl();
-  }
+  uint8_t _core;
 };
 
 // Policy for task with dynamic memory allocation
 template <uint32_t StackSize>
-struct DynamicPolicy : public Policy<DynamicPolicy<StackSize>> {
+class DynamicPolicy : public Policy<DynamicPolicy<StackSize>> {
+  public:
   static constexpr uint32_t _stack_size = StackSize;
 
   bool createImpl() {
@@ -108,10 +116,9 @@ struct DynamicPolicy : public Policy<DynamicPolicy<StackSize>> {
 
 // Policy for task with static memory allocation
 template <uint32_t StackSize>
-struct StaticPolicy : public Policy<StaticPolicy<StackSize>> {
+class StaticPolicy : public Policy<StaticPolicy<StackSize>> {
+  public:
   static constexpr uint32_t _stack_size = StackSize;
-  StackType_t _stack[StackSize];
-  StaticTask_t _tcb;
 
   bool createImpl() {
     this->_handle = xTaskCreateStaticPinnedToCore(this->_function,
@@ -125,6 +132,10 @@ struct StaticPolicy : public Policy<StaticPolicy<StackSize>> {
 
     return this->_handle != nullptr;
   }
+
+  private:
+  StackType_t _stack[StackSize];
+  StaticTask_t _tcb;
 };
 
 // Main task class. You need to specify the policy used
@@ -138,81 +149,87 @@ class Task : public ITask {
       , _stack_max(0) {}
 
   Task(const char* name, TaskFunction_t function, uint8_t priority, void* parameters,
-       uint8_t running_core = ARDUINO_RUNNING_CORE) {
-    _policy.create(name, function, parameters, priority, running_core);
+       uint8_t running_core = ARDUINO_RUNNING_CORE)
+      : _policy()
+      , _stack_used(0)
+      , _stack_min(0xffffffff)
+      , _stack_max(0) {
+    _policy.setup(name, function, priority, parameters, running_core);
   }
 
   ~Task() {
-    if (_policy._handle) vTaskDelete(_policy._handle);
+    if (_policy.getHandle()) vTaskDelete(_policy.getHandle());
   }
 
   bool create() { return _policy.create(); }
 
   bool create(const char* name, TaskFunction_t function, uint8_t priority, void* parameters,
               uint8_t running_core = ARDUINO_RUNNING_CORE) {
-    return _policy.create(name, function, parameters, priority, running_core);
+    _policy.setup(name, function, priority, parameters, running_core);
+    return _policy.create();
   }
 
-  bool isCreated() const { return _policy._handle != nullptr; }
+  bool isCreated() const { return _policy.getHandle() != nullptr; }
 
-  void suspend() const { vTaskSuspend(_policy._handle); }
+  void suspend() const { vTaskSuspend(_policy.getHandle()); }
 
-  void resume() const { vTaskResume(_policy._handle); }
+  void resume() const { vTaskResume(_policy.getHandle()); }
 
-  bool abortDelay() const { return xTaskAbortDelay(_policy._handle); }
+  eTaskState getState() const { return eTaskGetState(_policy.getHandle()); }
 
-  TaskHandle_t getHandle() const { return _policy._handle; }
+  bool abortDelay() const { return xTaskAbortDelay(_policy.getHandle()); }
 
-  const char* getName() const { return _policy._name; }
+  TaskHandle_t getHandle() const { return _policy.getHandle(); }
 
-  void* getParameters() const { return _policy._parameters; }
+  const char* getName() const { return _policy.getName(); }
 
-  void setPriority(const uint8_t priority) {
-    vTaskPrioritySet(_policy._handle, priority);
-    _policy._priority = priority;
-  }
+  void* getParameters() const { return _policy.getParameters(); }
 
-  uint8_t getPriority() const { return uxTaskPriorityGet(_policy._handle); }
+  uint8_t getCore() const { return _policy.getCore(); }
 
-  uint8_t getPriorityFromISR() const { return uxTaskPriorityGetFromISR(_policy._handle); }
+  void setPriority(const uint8_t priority) { vTaskPrioritySet(_policy.getHandle(), priority); }
+
+  uint8_t getPriority() const { return uxTaskPriorityGet(_policy.getHandle()); }
+
+  uint8_t getPriorityFromISR() const { return uxTaskPriorityGetFromISR(_policy.getHandle()); }
 
   uint32_t getStackSize() const { return Policy::_stack_size; }
 
   bool notify(const uint32_t value, const eNotifyAction action) {
-    return xTaskNotify(_policy._handle, value, action);
+    return xTaskNotify(_policy.getHandle(), value, action);
   }
 
   bool notifyFromISR(const uint32_t value, const eNotifyAction action,
                      BaseType_t& task_woken) const {
-    return xTaskNotifyFromISR(_policy._handle, value, action, &task_woken);
+    return xTaskNotifyFromISR(_policy.getHandle(), value, action, &task_woken);
   }
 
   bool notifyAndQuery(const uint32_t value, const eNotifyAction action, uint32_t& old_value) const {
-    return xTaskNotifyAndQuery(_policy._handle, value, action, &old_value);
+    return xTaskNotifyAndQuery(_policy.getHandle(), value, action, &old_value);
   }
 
   bool notifyAndQueryFromISR(const uint32_t value, const eNotifyAction action, uint32_t& old_value,
                              BaseType_t& task_woken) const {
-    return xTaskNotifyAndQueryFromISR(_policy._handle, value, action, &old_value, &task_woken);
+    return xTaskNotifyAndQueryFromISR(_policy.getHandle(), value, action, &old_value, &task_woken);
   }
 
-  bool notifyGive() const { return xTaskNotifyGive(_policy._handle); }
+  bool notifyGive() const { return xTaskNotifyGive(_policy.getHandle()); }
 
   void notifyGiveFromISR(BaseType_t& task_woken) const {
-    return vTaskNotifyGiveFromISR(_policy._handle, &task_woken);
+    return vTaskNotifyGiveFromISR(_policy.getHandle(), &task_woken);
   }
 
   uint32_t notifyTake(const bool clear, const TickType_t ticks_to_wait) const {
     return ulTaskNotifyTake(clear, ticks_to_wait);
   }
 
-  bool notifyWait(const uint32_t clear_on_entry, const uint32_t clear_on_exit,
-                  uint32_t* const value, const TickType_t ticks_to_wait) const {
-    return xTaskNotifyWait(clear_on_entry, clear_on_exit, value, ticks_to_wait);
+  bool notifyWait(const uint32_t clear_on_entry, const uint32_t clear_on_exit, uint32_t& value,
+                  const TickType_t ticks_to_wait) const {
+    return xTaskNotifyWait(clear_on_entry, clear_on_exit, &value, ticks_to_wait);
   }
 
   void updateStackStats() {
-    _stack_used = Policy::_stack_size - uxTaskGetStackHighWaterMark(_policy._handle);
+    _stack_used = Policy::_stack_size - uxTaskGetStackHighWaterMark(_policy.getHandle());
     _stack_min  = min(_stack_min, _stack_used);
     _stack_max  = max(_stack_max, _stack_used);
   }
@@ -223,7 +240,7 @@ class Task : public ITask {
 
   uint32_t getStackMaxUsed() const { return _stack_max; }
 
-  explicit operator bool() const { return _policy._handle != nullptr; }
+  explicit operator bool() const { return _policy.getHandle() != nullptr; }
 
   private:
   Policy _policy;
@@ -233,9 +250,9 @@ class Task : public ITask {
 };
 }; // namespace Internal
 
-template <uint32_t STACK_SIZE>
-using TaskDynamic = Internal::Task<Internal::DynamicPolicy<STACK_SIZE>>;
+template <uint32_t StackSize>
+using TaskDynamic = Internal::Task<Internal::DynamicPolicy<StackSize>>;
 
-template <uint32_t STACK_SIZE>
-using TaskStatic = Internal::Task<Internal::StaticPolicy<STACK_SIZE>>;
+template <uint32_t StackSize>
+using TaskStatic = Internal::Task<Internal::StaticPolicy<StackSize>>;
 } // namespace RTOS::Tasks
