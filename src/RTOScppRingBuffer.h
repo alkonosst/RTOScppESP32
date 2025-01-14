b7b2a05d1fa0b8b2b6b7b471e168ca70bf3d74ba
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-01-13 23:19:42 -0300

    refactor: Add getSize methods, cast correctly, fix buf size calculation, modify template parameters

--- src/RTOScppRingBuffer.h
+++ src/RTOScppRingBuffer.h
@@ -26,6 +26,9 @@ class IRingBuffer {
 
   virtual RingbufHandle_t getHandle() const = 0;
 
+  virtual uint32_t getMaxItemSize() const = 0;
+  virtual uint32_t getFreeSize() const    = 0;
+
   virtual explicit operator bool() const = 0;
   friend bool operator==(const QueueSetMemberHandle_t& queue_set_member,
                          const IRingBuffer& ringbuf);
@@ -46,17 +49,17 @@ class Policy {
 
   bool send(const T* const item, const uint32_t item_size,
             const TickType_t ticks_to_wait = portMAX_DELAY) const {
-    return xRingbufferSend(this->_handle, (void*)item, item_size, ticks_to_wait);
+    return xRingbufferSend(this->_handle, item, item_size, ticks_to_wait);
   }
 
   bool sendFromISR(const T* const item, const uint32_t item_size, BaseType_t& task_woken) const {
-    return xRingbufferSendFromISR(this->_handle, (void*)item, item_size, &task_woken);
+    return xRingbufferSendFromISR(this->_handle, item, item_size, &task_woken);
   }
 
-  void returnItem(const T* const item) const { vRingbufferReturnItem(this->_handle, (void*)item); }
+  void returnItem(T* const item) const { vRingbufferReturnItem(this->_handle, item); }
 
-  void returnItemFromISR(const T* const item, BaseType_t& task_woken) const {
-    vRingbufferReturnItemFromISR(this->_handle, (void*)item, &task_woken);
+  void returnItemFromISR(T* const item, BaseType_t& task_woken) const {
+    vRingbufferReturnItemFromISR(this->_handle, item, &task_woken);
   }
 
   protected:
@@ -70,15 +73,12 @@ class Policy {
 template <typename Derived, typename T>
 class NoSplitPolicy : public Policy<Derived, T> {
   public:
-  // Size aligned to nearest 4 bytes + 8 bytes per item
-  static constexpr uint32_t ALIGNED_SIZE = 4 * ((sizeof(T) + 3) / 4) + 8;
-
   T* receive(uint32_t& item_size, const TickType_t ticks_to_wait = portMAX_DELAY) {
-    return xRingbufferReceive(this->_handle, &item_size, ticks_to_wait);
+    return static_cast<T*>(xRingbufferReceive(this->_handle, &item_size, ticks_to_wait));
   }
 
   T* receiveFromISR(uint32_t& item_size) {
-    return xRingbufferReceiveFromISR(this->_handle, &item_size);
+    return static_cast<T*>(xRingbufferReceiveFromISR(this->_handle, &item_size));
   }
 };
 
@@ -86,36 +86,36 @@ class NoSplitPolicy : public Policy<Derived, T> {
 template <typename T, uint32_t Length>
 class NoSplitDynamicPolicy : public NoSplitPolicy<NoSplitDynamicPolicy<T, Length>, T> {
   public:
-  NoSplitDynamicPolicy() {
-    this->_handle =
-      xRingbufferCreate(Length * NoSplitPolicy<NoSplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE,
-                        RINGBUF_TYPE_NOSPLIT);
-  }
+  NoSplitDynamicPolicy() { this->_handle = xRingbufferCreate(Length, RINGBUF_TYPE_NOSPLIT); }
 };
 
 // Policy for no-split ring buffer with static memory allocation
 template <typename T, uint32_t Length>
 class NoSplitStaticPolicy : public NoSplitPolicy<NoSplitStaticPolicy<T, Length>, T> {
+  private:
+  // Size aligned to nearest 4 bytes
+  static constexpr uint32_t REQUIRED_SIZE = 4 * ((Length + 3) / 4);
+
   public:
   NoSplitStaticPolicy() {
-    this->_handle = xRingbufferCreateStatic(
-      Length * NoSplitPolicy<NoSplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE,
-      RINGBUF_TYPE_NOSPLIT,
-      _storage,
-      &_tcb);
+    this->_handle = xRingbufferCreateStatic(REQUIRED_SIZE, RINGBUF_TYPE_NOSPLIT, _storage, &_tcb);
   }
 
   private:
   StaticRingbuffer_t _tcb;
-  uint8_t _storage[Length * NoSplitPolicy<NoSplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE];
+  uint8_t _storage[REQUIRED_SIZE];
 };
 
 // Policy for no-split ring buffer with external storage
-template <typename T>
-class NoSplitExternalStoragePolicy : public NoSplitPolicy<NoSplitExternalStoragePolicy<T>, T> {
+template <typename T, uint32_t Length>
+class NoSplitExternalStoragePolicy
+    : public NoSplitPolicy<NoSplitExternalStoragePolicy<T, Length>, T> {
   public:
-  bool create(uint8_t* const buffer, const uint32_t length) {
-    this->_handle = xRingbufferCreateStatic(length, RINGBUF_TYPE_NOSPLIT, buffer, &_tcb);
+  // Size aligned to nearest 4 bytes
+  static constexpr uint32_t REQUIRED_SIZE = 4 * ((Length + 3) / 4);
+
+  bool create(uint8_t* const buffer) {
+    this->_handle = xRingbufferCreateStatic(REQUIRED_SIZE, RINGBUF_TYPE_NOSPLIT, buffer, &_tcb);
     return this->_handle != nullptr;
   }
 
@@ -127,56 +127,65 @@ class NoSplitExternalStoragePolicy : public NoSplitPolicy<NoSplitExternalStorage
 template <typename Derived, typename T>
 class SplitPolicy : public Policy<Derived, T> {
   public:
-  // Size aligned to nearest 4 bytes + 8 bytes per item
-  static constexpr uint32_t ALIGNED_SIZE = 4 * ((sizeof(T) + 3) / 4) + 8;
-
-  bool receive(T** head, T** tail, uint32_t& head_item_size, uint32_t& tail_item_size,
+  bool receive(T*& head, T*& tail, uint32_t& head_item_size, uint32_t& tail_item_size,
                const TickType_t ticks_to_wait = portMAX_DELAY) {
-    return xRingbufferReceiveSplit(
-      this->_handle, head, tail, &head_item_size, &tail_item_size, ticks_to_wait);
+    return xRingbufferReceiveSplit(this->_handle,
+                                   reinterpret_cast<void**>(&head),
+                                   reinterpret_cast<void**>(&tail),
+                                   &head_item_size,
+                                   &tail_item_size,
+                                   ticks_to_wait);
   }
 
-  bool receiveFromISR(T** head, T** tail, uint32_t& head_item_size, uint32_t& tail_item_size) {
-    return xRingbufferReceiveSplitFromISR(
-      this->_handle, head, tail, &head_item_size, &tail_item_size);
+  bool receiveFromISR(T*& head, T*& tail, uint32_t& head_item_size, uint32_t& tail_item_size) {
+    return xRingbufferReceiveSplitFromISR(this->_handle,
+                                          reinterpret_cast<void**>(&head),
+                                          reinterpret_cast<void**>(&tail),
+                                          &head_item_size,
+                                          &tail_item_size);
   }
 };
 
 // Policy for split ring buffer with dynamic memory allocation
 template <typename T, uint32_t Length>
 class SplitDynamicPolicy : public SplitPolicy<SplitDynamicPolicy<T, Length>, T> {
+  private:
+  // Size aligned to nearest 4 bytes
+  static constexpr uint32_t REQUIRED_SIZE = 4 * ((Length + 3) / 4);
 
   public:
   SplitDynamicPolicy() {
-    this->_handle =
-      xRingbufferCreate(Length * SplitPolicy<SplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE,
-                        RINGBUF_TYPE_ALLOWSPLIT);
+    this->_handle = xRingbufferCreate(REQUIRED_SIZE, RINGBUF_TYPE_ALLOWSPLIT);
   }
 };
 
 // Policy for split ring buffer with static memory allocation
 template <typename T, uint32_t Length>
 class SplitStaticPolicy : public SplitPolicy<SplitStaticPolicy<T, Length>, T> {
+  private:
+  // Size aligned to nearest 4 bytes
+  static constexpr uint32_t REQUIRED_SIZE = 4 * ((Length + 3) / 4);
+
   public:
   SplitStaticPolicy() {
     this->_handle =
-      xRingbufferCreateStatic(Length * SplitPolicy<SplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE,
-                              RINGBUF_TYPE_ALLOWSPLIT,
-                              _storage,
-                              &_tcb);
+      xRingbufferCreateStatic(REQUIRED_SIZE, RINGBUF_TYPE_ALLOWSPLIT, _storage, &_tcb);
   }
 
   private:
   StaticRingbuffer_t _tcb;
-  uint8_t _storage[Length * SplitPolicy<SplitDynamicPolicy<T, Length>, T>::ALIGNED_SIZE];
+  uint8_t _storage[REQUIRED_SIZE];
 };
 
 // Policy for split ring buffer with external storage
-template <typename T>
-class SplitExternalStoragePolicy : public SplitPolicy<SplitExternalStoragePolicy<T>, T> {
+template <typename T, uint32_t Length>
+class SplitExternalStoragePolicy : public SplitPolicy<SplitExternalStoragePolicy<T, Length>, T> {
   public:
-  bool create(uint8_t* const buffer, const uint32_t length) {
-    this->_handle = xRingbufferCreateStatic(length, RINGBUF_TYPE_ALLOWSPLIT, buffer, &_tcb);
+  // Size aligned to nearest 4 bytes
+  static constexpr uint32_t REQUIRED_SIZE = 4 * ((Length + 3) / 4);
+
+  bool create(uint8_t* const buffer) {
+    this->_handle = xRingbufferCreateStatic(REQUIRED_SIZE, RINGBUF_TYPE_ALLOWSPLIT, buffer, &_tcb);
     return this->_handle != nullptr;
   }
 
@@ -190,11 +199,13 @@ class BytePolicy : public Policy<Derived, uint8_t> {
   public:
   uint8_t* receiveUpTo(const uint32_t max_item_size, uint32_t& item_size,
                        const TickType_t ticks_to_wait = portMAX_DELAY) {
-    return xRingbufferReceiveUpTo(this->_handle, &item_size, ticks_to_wait, max_item_size);
+    return static_cast<uint8_t*>(
+      xRingbufferReceiveUpTo(this->_handle, &item_size, ticks_to_wait, max_item_size));
   }
 
   uint8_t* receiveUpToFromISR(const uint32_t max_item_size, uint32_t& item_size) {
-    return xRingbufferReceiveUpToFromISR(this->_handle, &item_size, max_item_size);
+    return static_cast<uint8_t*>(
+      xRingbufferReceiveUpToFromISR(this->_handle, &item_size, max_item_size));
   }
 };
 
@@ -219,11 +230,13 @@ class ByteStaticPolicy : public BytePolicy<ByteStaticPolicy<Length>> {
 };
 
 // Policy for byte ring buffer with external storage
-template <uint8_t Dummy = 0>
-class ByteExternalStoragePolicy : public BytePolicy<ByteExternalStoragePolicy<Dummy>> {
+template <uint32_t Length>
+class ByteExternalStoragePolicy : public BytePolicy<ByteExternalStoragePolicy<Length>> {
   public:
-  bool create(uint8_t* const buffer, const uint32_t length) {
-    this->_handle = xRingbufferCreateStatic(length, RINGBUF_TYPE_BYTEBUF, buffer, &_tcb);
+  static constexpr uint32_t REQUIRED_SIZE = Length;
+
+  bool create(uint8_t* const buffer) {
+    this->_handle = xRingbufferCreateStatic(Length, RINGBUF_TYPE_BYTEBUF, buffer, &_tcb);
     return this->_handle != nullptr;
   }
 
@@ -238,12 +251,15 @@ class RingBuffer : public IRingBuffer, public Policy {
   using Policy::Policy; // Inherit constructor
 
   ~RingBuffer() {
-    if (Policy::getHandle()) vRingbufferDelete(Policy::getHandle());
+    if (getHandle()) vRingbufferDelete(getHandle());
   }
 
   RingbufHandle_t getHandle() const override { return Policy::getHandle(); }
 
-  explicit operator bool() const override { return Policy::getHandle() != nullptr; }
+  uint32_t getMaxItemSize() const override { return xRingbufferGetMaxItemSize(getHandle()); }
+  uint32_t getFreeSize() const override { return xRingbufferGetCurFreeSize(getHandle()); }
+
+  explicit operator bool() const override { return getHandle() != nullptr; }
 };
 
 } // namespace Internal
@@ -254,9 +270,9 @@ using RingBufferNoSplitDynamic = Internal::RingBuffer<Internal::NoSplitDynamicPo
 template <typename T, uint32_t Length>
 using RingBufferNoSplitStatic = Internal::RingBuffer<Internal::NoSplitStaticPolicy<T, Length>>;
 
-template <typename T>
+template <typename T, uint32_t Length>
 using RingBufferNoSplitExternalStorage =
-  Internal::RingBuffer<Internal::NoSplitExternalStoragePolicy<T>>;
+  Internal::RingBuffer<Internal::NoSplitExternalStoragePolicy<T, Length>>;
 
 template <typename T, uint32_t Length>
 using RingBufferSplitDynamic = Internal::RingBuffer<Internal::SplitDynamicPolicy<T, Length>>;
@@ -264,9 +280,9 @@ using RingBufferSplitDynamic = Internal::RingBuffer<Internal::SplitDynamicPolicy
 template <typename T, uint32_t Length>
 using RingBufferSplitStatic = Internal::RingBuffer<Internal::SplitStaticPolicy<T, Length>>;
 
-template <typename T>
+template <typename T, uint32_t Length>
 using RingBufferSplitExternalStorage =
-  Internal::RingBuffer<Internal::SplitExternalStoragePolicy<T>>;
+  Internal::RingBuffer<Internal::SplitExternalStoragePolicy<T, Length>>;
 
 template <uint32_t Length>
 using RingBufferByteDynamic = Internal::RingBuffer<Internal::ByteDynamicPolicy<Length>>;
@@ -274,6 +290,8 @@ using RingBufferByteDynamic = Internal::RingBuffer<Internal::ByteDynamicPolicy<L
 template <uint32_t Length>
 using RingBufferByteStatic = Internal::RingBuffer<Internal::ByteStaticPolicy<Length>>;
 
-using RingBufferByteExternalStorage = Internal::RingBuffer<Internal::ByteExternalStoragePolicy<>>;
+template <uint32_t Length>
+using RingBufferByteExternalStorage =
+  Internal::RingBuffer<Internal::ByteExternalStoragePolicy<Length>>;
 
 } // namespace RTOS::RingBuffers
